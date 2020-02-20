6b016f63b1a0dc99d309ab5969b74dbcad99c572
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2020-02-20 17:30:12 +0100

    Update basic layout

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
+      { 'http-equiv': 'content-type', content: 'text/html; charset=UTF-8' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
