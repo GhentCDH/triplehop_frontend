0b476116ac44ae361d03bb9a61ca6207f73ad8d5
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2020-07-08 17:10:04 +0200

    Add blank project home page

---
+++ pages/_project_name/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>
+      {{ projectDisplayName }} home page
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  auth: false,
+  computed: {
+    projectDisplayName () {
+      return this.$store.state.config.project_def.display_name
+    }
+  }
+}
+</script>
