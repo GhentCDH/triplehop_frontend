4d9f73fe6e7bf7e26e36cfb71ae31473b5dda622
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2020-10-09 21:26:52 +0200

    Hide relations if there are more than 5

--- components/RelationData.vue
+++ components/RelationData.vue
@@ -5,11 +5,11 @@
     class="border-0 bg-light mb-3"
   >
     <b-card
-      v-for="relation in relationData"
+      v-for="relation in relations"
       :key="relation.id"
       class="border-0 bg-white mb-1"
     >
-      <nuxt-link
+      <b-link
         :to="{
           name: 'project_name-entity_type_name-id',
           params: {
@@ -20,8 +20,16 @@
         }"
       >
         {{ constructTitle(entityTypesConfig[relation.entity.__typename.toLowerCase()].display.title, relation.entity) }}
-      </nuxt-link>
+      </b-link>
     </b-card>
+    <b-button
+      v-if="relationData.length > 5"
+      class="mt-3"
+      variant="primary"
+      @click="collapsed = !collapsed"
+    >
+      {{ buttonText }}
+    </b-button>
   </b-card>
 </template>
 <script>
@@ -29,6 +37,10 @@ import { constructTitle } from '~/assets/js/utils'
 
 export default {
   props: {
+    collapsed: {
+      type: Boolean,
+      default: true
+    },
     entityTypesConfig: {
       type: Object,
       required: true
@@ -44,6 +56,23 @@ export default {
     relationTitle: {
       type: String,
       required: true
+    }
+  },
+  computed: {
+    buttonText () {
+      if (this.collapsed) {
+        return `Show ${this.relationData.length - 3} more results (${this.relationData.length} in total)`
+      }
+      return `Hide ${this.relationData.length - 3} results (${this.relationData.length} in total)`
+    },
+    firstThree () {
+      return this.relationData.slice(0, 3)
+    },
+    relations () {
+      if (this.relationData.length > 5 && this.collapsed) {
+        return this.firstThree
+      }
+      return this.relationData
     }
   },
   methods: {
