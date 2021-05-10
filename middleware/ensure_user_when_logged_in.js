c6e59cedd373f2500b12448833ed60250c6f43a9
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2021-05-10 14:30:02 +0200

    Fix user getting middleware

--- middleware/ensure_user_when_logged_in.js
+++ middleware/ensure_user_when_logged_in.js
@@ -1,9 +1,10 @@
 export default async function ({ $axios, $auth }) {
   if ($auth.loggedIn && ($auth.user == null || Object.keys($auth.user).length === 0)) {
-    const response = await $axios.get('/auth/user')
-    if (response.status !== 200) {
-      throw new Error('Could not get user data')
+    try {
+      const response = await $axios.get('/auth/user')
+      $auth.setUser(response.data)
+    } catch (error) {
+      $auth.logout()
     }
-    $auth.setUser(response.data)
   }
 }
