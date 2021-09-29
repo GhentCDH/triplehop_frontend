9cc505552bde10872890e38251ce029064cad477
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2021-09-29 11:21:19 +0200

    Add field component to display filmmagie files

---
+++ components/FilmmagieField.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-row
+      v-for="(valueAndSource, index) in valueAndSources"
+      :key="index"
+    >
+      <b-col md="3">
+        <b-img
+          fluid
+          :src="`https://resolver.libis.be/${valueAndSource.value}/thumbnail`"
+        />
+      </b-col>
+      <b-col md="9">
+        <b-link
+          :href="`https://resolver.libis.be/${valueAndSource.value}/representation`"
+          target="_blank"
+        >
+          <b-icon icon="box-arrow-up-right" />
+          View complete source
+        </b-link>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    valueAndSources: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
