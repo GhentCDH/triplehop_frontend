0de7acc42b01a250ac2fdb874b08b247b50ff80e
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2021-10-29 11:52:52 +0200

    Add '__all__' permission

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,43 +1,72 @@
 export function hasGlobalPermission (user, permission) {
-  if (
-    permission in user.permissions &&
-    '__all__' in user.permissions[permission]
-  ) {
-    return true
+  if (user == null || user === false) {
+    return false
   }
+
+  // Check for specific or all on permission level
+  for (const perm of ['__all__', permission]) {
+    if (
+      perm in user.permissions &&
+      '__all__' in user.permissions[perm]
+    ) {
+      return true
+    }
+  }
+
+  return false
 }
 
 export function hasOneOfGlobalPermissions (user, permissions) {
+  if (user == null || user === false) {
+    return false
+  }
+
   for (const permission of permissions) {
     if (hasGlobalPermission(user, permission)) {
       return true
     }
   }
+
   return false
 }
 
 export function hasGlobalAdminAccess (user) {
+  if (user == null) {
+    return false
+  }
+
   return hasOneOfGlobalPermissions(user, [])
 }
 
 export function hasProjectPermission (user, projectName, permission) {
+  if (user == null || user === false) {
+    return false
+  }
+
   if (projectName == null) {
     return false
   }
 
-  if (
-    permission in user.permissions && (
-      '__all__' in user.permissions[permission] ||
-      projectName in user.permissions[permission]
-    )
-  ) {
-    return true
+  // Check for specific or all on permission, project level
+  for (const perm of ['__all__', permission]) {
+    for (const proj of ['__all__', projectName]) {
+      if (
+        perm in user.permissions &&
+        proj in user.permissions[perm]
+      ) {
+        return true
+      }
+    }
   }
 
   return false
 }
 
 export function hasOneOfProjectPermissions (user, projectName, permissions) {
+  if (user == null || user === false) {
+    return false
+  }
+
   if (projectName == null) {
     return false
   }
@@ -56,7 +85,11 @@ export function hasProjectAdminAccess (user, projectName) {
 }
 
 export function hasEntityPermission (user, projectName, entityName, permission) {
-  if (!hasProjectPermission(user, projectName, permission)) {
+  if (user == null || user === false) {
+    return false
+  }
+
+  if (projectName == null) {
     return false
   }
 
@@ -64,22 +97,19 @@ export function hasEntityPermission (user, projectName, entityName, permission) 
     return false
   }
 
-  if (
-    '__all__' in user.permissions[permission] && (
-      '__all__' in user.permissions[permission].__all__.entities ||
-      entityName in user.permissions[permission].__all__.entities
-    )
-  ) {
-    return true
-  }
-
-  if (
-    projectName in user.permissions[permission] && (
-      '__all__' in user.permissions[permission][projectName].entities ||
-      entityName in user.permissions[permission][projectName].entities
-    )
-  ) {
-    return true
+  // Check for specific or all on permission, project, entity level
+  for (const perm of ['__all__', permission]) {
+    for (const proj of ['__all__', projectName]) {
+      for (const ent of ['__all__', entityName]) {
+        if (
+          perm in user.permissions &&
+          proj in user.permissions[perm] &&
+          ent in user.permissions[perm][proj].entities
+        ) {
+          return true
+        }
+      }
+    }
   }
 
   return false
