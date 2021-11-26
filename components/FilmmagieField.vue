1576f7233cee5e8111c60cd5d05c0a8a2132eaed
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2021-11-26 11:55:30 +0100

    Display sources for filmmagie id

--- components/FilmmagieField.vue
+++ components/FilmmagieField.vue
@@ -10,7 +10,7 @@
           :src="`https://resolver.libis.be/${valueAndSource.value}/thumbnail`"
         />
       </b-col>
-      <b-col md="9">
+      <b-col md="3">
         <b-link
           :href="`https://resolver.libis.be/${valueAndSource.value}/representation`"
           target="_blank"
@@ -19,11 +19,22 @@
           View complete source
         </b-link>
       </b-col>
+      <b-col md="6">
+        <sources :sources="valueAndSource.sources" />
+      </b-col>
     </b-row>
   </div>
 </template>
 <script>
+import frag from 'vue-frag'
+import Sources from '~/components/Sources.vue'
 export default {
+  directives: {
+    frag
+  },
+  components: {
+    Sources
+  },
   props: {
     valueAndSources: {
       type: Array,
