0276f9b5c226a5c5e64b84f69af37dfdc40b386a
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2022-05-10 14:20:48 +0200

    Fix html to prevent hydration error

--- components/Edit/RelationEditPanel.vue
+++ components/Edit/RelationEditPanel.vue
@@ -3,21 +3,26 @@
     :title="panelTitle"
     class="bg-light border-0 mb-3"
   >
+    <!-- todo: use relation type and relation id as key below -->
     <b-card
       v-for="(fd, index) in formData"
       :key="index"
       class="border-0 bg-white mb-1"
     >
-      <p>
-        [{{ fd.entity.id }}] {{ fd.entity.title }}
-        <!-- TODO: check edit permission on entity type -->
-        <b-link
-          class="float-right"
-        >
-          <b-icon-pencil-fill />
-          Edit entity
-        </b-link>
-      </p>
+      <b-row>
+        <b-col sm="9">
+          [{{ fd.entity.id }}] {{ fd.entity.title }}
+          <!-- TODO: check edit permission on entity type -->
+        </b-col>
+        <b-col sm="3">
+          <b-link
+            class="float-right"
+          >
+            <b-icon-pencil-fill />
+            Edit entity
+          </b-link>
+        </b-col>
+      </b-row>
       <b-button
         class="float-right"
         variant="danger"
