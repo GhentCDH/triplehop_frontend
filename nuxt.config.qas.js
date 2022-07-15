4f180f60654927ce5d14809230d757ac14589193
Pieterjan De Potter <pieterjan.depotter@ugent.be>
2022-07-15 23:10:16 +0200

    Make sure basic authentication isn't forwarded to the back-end (issue with fastapi_jwt_auth)

--- nuxt.config.qas.js
+++ nuxt.config.qas.js
@@ -1,5 +1,6 @@
 import config from './nuxt.config.js'
 
 config.axios.baseURL = 'https://qas.triplehop-backend.ugent.be/'
+config.axios.proxyHeadersIgnore = ['accept', 'host', 'x-forwarded-host', 'x-forwarded-port', 'x-forwarded-proto', 'cf-ray', 'cf-connecting-ip', 'content-length', 'content-md5', 'content-type', 'authorization']
 
 export default config
